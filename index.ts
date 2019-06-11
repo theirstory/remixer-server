4a8d4ba34f3466c32ca3f15055b3025b38699659
Edward Silverton <e.silverton@gmail.com>
2019-06-11 12:13:27 +0100

    added duration

--- index.ts
+++ index.ts
@@ -1,20 +1,30 @@
 const express = require("express");
+var cors = require("cors")
 const app = express();
 const dotenv = require("dotenv");
 const http = require("http");
-import { listVideos } from "./videos";
+import { list, duration } from "./videos";
 dotenv.config();
 
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/videos", async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  const videos = await listVideos();
+app.get("/list", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  const videos = await list();
   res.end(JSON.stringify(videos));
+});
+
+app.get("/duration/:video", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  const d: number = await duration(req.params.video);
+  res.end(JSON.stringify(d));
 });
 
 // Start the express application
--- videos.ts
+++ videos.ts
@@ -1,7 +1,10 @@
 import { promise as glob } from "glob-promise";
+import { basename } from "path";
+import ffprobe from "ffprobe";
+import ffprobeStatic from "ffprobe-static";
 
-export const listVideos = async () => {
-  const videosPattern = "./videos/*";
+export const list = async () => {
+  const videosPattern = "./public/videos/*.mp4";
 
   const videos = await glob(videosPattern, {
     ignore: []
@@ -10,8 +13,18 @@ export const listVideos = async () => {
   const response = [];
 
   await Promise.all(videos.map(async (video) => {
-    response.push(video);
+    response.push(basename(video));
   }));
 
   return response;
+}
+
+export const duration = async (video: string) => {
+  video = "./public/videos/" + video;
+  const info: any = await ffprobe(video, { path: ffprobeStatic.path });
+  if (info && info.streams && info.streams.length) {
+      const duration: number = Number(info.streams[0].duration);
+      return duration;
+  }
+  return 0;
 }
